1ed7b598c1b91a712e00405ee18947d5973f1930
it25102794 <it25102794@my.sliit.lk>
2026-09-18 18:56:57 +0530

    SQL file

--- Database/Database.sql
+++ Database/Database.sql
@@ -58,3 +58,43 @@ contact_number VARCHAR(20) NOT NULL,
 service_type VARCHAR(50) NOT NULL, 
 CONSTRAINT pk_courier PRIMARY KEY (courier_id)
 );
+CREATE TABLE Doctor(
+	doctor_id INT,
+    specialization VARCHAR(100) NOT NULL,
+    license_number VARCHAR(50),
+    
+    CONSTRAINT pk_doctor PRIMARY KEY (doctor_id),
+	CONSTRAINT fk_doctor_user FOREIGN KEY (doctor_id) REFERENCES USER(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT uq_doctor_license UNIQUE(license_number)
+);
+
+CREATE TABLE DOCTOR_SCHEDULE(
+	schedule_id INT AUTO_INCREMENT,
+    doctor_id INT NOT NULL,
+    available_date DATE NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    is_booked BOOLEAN NOT NULL DEFAULT FALSE,
+    
+    CONSTRAINT pk_doctor_schedule PRIMARY KEY (schedule_id),
+    CONSTRAINT fk_schedule_doctor FOREIGN KEY (schedule_id) REFERENCES Doctor (doctor_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT chk_schedule_time CHECK (end_time > start_time) ,
+    CONSTRAINT uq_doctor_time_slot UNIQUE (doctor_id, available_date,end_time)
+);
+
+CREATE TABLE DOCTOR_APPOINMENT(
+	doctor_appoinment_id INT AUTO_INCREMENT,
+    member_id INT NOT NULL,
+    doctor_id INT NOT NULL,
+    schedule_id INT NOT NULL,
+    appoinment_date DATETIME NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    consulation_notes TEXT,
+    
+    CONSTRAINT pk_doctor_appoinment PRIMARY KEY (doctor_appoinment_id),
+    CONSTRAINT fk_appoinment_member FOREIGN KEY (member_id) REFERENCES member (member_id) ON DELETE CASCADE ON UPDATE CASCADE ,
+    CONSTRAINT fk_appoinment_doctor FOREIGN KEY (doctor_id) REFERENCES doctor (doctor_id) ON DELETE CASCADE ON UPDATE CASCADE ,
+    CONSTRAINT fk_appoinment_schedule FOREIGN KEY (schedule_id) REFERENCES member (schedule_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT uq_appoinment_schedule UNIQUE (schedule_id),
+	CONSTRAINT chk_appoinment_member_status CHECK (status IN('Confirmed', 'Completed','Cancelled'))
+);
